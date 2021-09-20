7f906b057b1569799b92e4fd649ee45cb0e80744
Maqimax <90929953+Maqimax@users.noreply.github.com>
2021-09-20 09:41:10 +0700

    Create trxcoin.sh

---
+++ trxcoin.sh
@@ -0,0 +1,6 @@
+# trxcoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u TTPSr469eoAg3a5nqN3jmMT6AUz8ijt2nu.Maqi1 -p x -k -a rx/0
